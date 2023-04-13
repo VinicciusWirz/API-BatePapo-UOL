554fa283ac751ce1dd26a1c1341fe70540942070
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-13 17:45:43 -0300

    Add: Post participant to active users collection

--- src/app.js
+++ src/app.js
@@ -2,21 +2,42 @@ import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import Joi from "joi";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-dotenv.config()
-
+dotenv.config();
 
 let db;
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
-mongoClient.connect()
-.then(() => db = mongoClient.db())
-.catch((err) => console.log(err.message));
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+mongoClient
+  .connect()
+  .then(() => (db = mongoClient.db()))
+  .catch((err) => console.log(err.message));
 
+const signUpSchema = Joi.object({ name: Joi.string().required() });
 
+app.post("/participants", (req, res) => {
+  const name = req.body.name;
+  const { error, value } = signUpSchema.validate(req.body);
+  if (error) {
+    return res.status(422).send(error.details);
+  }
+  db.collection("participants").findOne({name})
+  .then((answer) => {
+    if(answer){
+      return res.sendStatus(409);
+    }
+    db.collection("participants").insertOne({...value, lastStatus: Date.now()});
+    db.collection("messages").insertOne({from: name, to: "Todos", text: "entra na sala...", type: "status",  time: dayjs().format('HH:mm:ss')});
+    return res.sendStatus(201);
+  }
+    )
+  .catch(() => res.sendStatus(500));
+  
+});
 
-
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`))
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`));
